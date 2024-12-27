1f9c3cc0cc37c93924fea7defa72e217849d31a2
Shubham.Sharma <shubham.sharma@inveniolsi.com>
2024-12-27 14:30:58 +0530

    Initial commit

---
+++ src/main/java/jenkins_ci_cd/jenkins/Controller.java
@@ -0,0 +1,17 @@
+package jenkins_ci_cd.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping("/hello")
+    public String fun1()
+    {
+        return "hello world";
+    }
+}
---
+++ src/test/java/jenkins_ci_cd/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package jenkins_ci_cd.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
